1a9e852ce876404e16b33e67c9685c58ed2990fc
Chen Yang <yangcnju@gmail.com>
2021-09-05 23:34:13 -0400

    feat(hiearchy): basic tdd code structure

--- backend/application/api.py
+++ backend/application/api.py
@@ -3,15 +3,23 @@ import sys
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-sys.path.insert(0, os.path.dirname(__file__))
-from database.models import setup_db, db, User, Card, Category
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+from application.models import setup_db, db, migrate, User, Card, Category
 
 
-def create_app(test_config=None):
+def create_app(config_name):
   app = Flask(__name__)
-  setup_db(app)
+  config_module = f"application.config.{config_name.capitalize()}Config"
+  app.config.from_object(config_module)
+  db.init_app(app)
+  migrate.init_app(app, db)
+
 
   CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+  @app.route("/")
+  def hello_world():
+    return "Hello, World!"
 
   @app.after_request
   def after_request(response):
@@ -21,7 +29,7 @@ def create_app(test_config=None):
       'Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     return response
 
-  @app.route('/transactions', method=['GET'])
+  @app.route('/transactions', methods=['GET'])
   def get_transactions():
     pass
 
@@ -33,75 +41,75 @@ def create_app(test_config=None):
   def post_transaction(transaction_id:int):
     pass
 
-  @app.route('transactions/<int:transaction_id>', method=['PATCH'])
+  @app.route('/transactions/<int:transaction_id>', methods=['PATCH'])
   def patch_transaction(transaction_id:int):
     pass
 
-  @app.route('transactions/<int:transaction_id>', method=['DELETE'])
+  @app.route('/transactions/<int:transaction_id>', methods=['DELETE'])
   def delete_transaction(transaction_id:int):
     pass
 
-  @app.route('/cards', method=['GET'])
+  @app.route('/cards', methods=['GET'])
   def get_cards():
     pass
 
-  @app.route('/cards/<int:card_id>', method=['GET'])
+  @app.route('/cards/<int:card_id>', methods=['GET'])
   def get_card(card_id:int):
     pass
 
-  @app.route('/cards', method=['POST'])
+  @app.route('/cards', methods=['POST'])
   def post_card(card_id:int):
     pass
 
-  @app.route('/cards/<int:card_id>', method=['DELETE'])
+  @app.route('/cards/<int:card_id>', methods=['DELETE'])
   def delete_card(card_id:int):
     pass
 
-  @app.route('/categories', method=['GET'])
+  @app.route('/categories', methods=['GET'])
   def get_categories():
     pass
 
-  @app.route('/categories/<int:category_id>', method=['GET'])
+  @app.route('/categories/<int:category_id>', methods=['GET'])
   def get_category(category_id:int):
     pass
 
-  @app.route('/categories', method=['POST'])
+  @app.route('/categories', methods=['POST'])
   def post_category(category_id:int):
     pass
 
-  @app.route('/categories/<int:category_id>', method=['DELETE'])
+  @app.route('/categories/<int:category_id>', methods=['DELETE'])
   def delete_category(category_id:int):
     pass
 
-  @app.route('/currencies', method=['GET'])
+  @app.route('/currencies', methods=['GET'])
   def get_currencies():
     pass
 
-  @app.route('/currencies/<int:currency_id>', method=['GET'])
+  @app.route('/currencies/<int:currency_id>', methods=['GET'])
   def get_currency(currency_id:int):
     pass
 
-  @app.route('/currencies', method=['POST'])
+  @app.route('/currencies', methods=['POST'])
   def post_currency(currency_id:int):
     pass
 
-  @app.route('/currencies/<int:currency_id>', method=['DELETE'])
+  @app.route('/currencies/<int:currency_id>', methods=['DELETE'])
   def delete_currency(currency_id:int):
     pass
 
-  @app.route('/users', method=['GET'])
+  @app.route('/users', methods=['GET'])
   def get_users():
     pass
 
-  @app.route('/users/<int:user_id>', method=['GET'])
+  @app.route('/users/<int:user_id>', methods=['GET'])
   def get_user(user_id:int):
     pass
 
-  @app.route('/users', method=['POST'])
+  @app.route('/users', methods=['POST'])
   def post_user(user_id:int):
     pass
 
-  @app.route('/users/<int:user_id>', method=['DELETE'])
+  @app.route('/users/<int:user_id>', methods=['DELETE'])
   def delete_user(user_id:int):
     pass
 
@@ -131,3 +139,20 @@ def create_app(test_config=None):
 
   return app
 
+
+if __name__ == '__main__':
+  database_name = "postgres"
+  database_path = "postgresql://{}:{}@{}/{}".format(
+    'postgres', 'postgres',
+    'localhost:6432', database_name)
+  app = create_app()
+  # setup_db(app, database_path)
+
+  # binds the app to the current context
+  # with app.app_context():
+  #   db = SQLAlchemy()
+  #   db.init_app(app)
+  #   # create all tables
+  #   db.create_all()
+  
+  app.run(host='127.0.0.1')
